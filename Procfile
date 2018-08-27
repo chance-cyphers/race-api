032e82c66887f4b6bb9b87d0f2f77a024774b3ee
chance <chance.a.cyphers@gmail.com>
2018-08-27 19:33:41 -0400

    Fix heroku build

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: java $JVM_OPTS -jar target/race-api-standalone.jar
-release: lein uberjar
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject race-api "0.0.1"
   :description "This will help you race"
   :url "http://chance.pants.com"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
