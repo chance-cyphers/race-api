5531df1339fac7411f275149b938610f6db47da2
chance <chance.a.cyphers@gmail.com>
2018-08-27 20:27:05 -0400

    Fix migrations

--- src/lobos/config.clj
+++ src/lobos/config.clj
@@ -1,5 +1,19 @@
 (ns lobos.config
-  (:require [lobos.connectivity :as lobos]
+  (:require [lobos.connectivity]
             [race-api.db.database :as db]))
 
-(lobos/open-global db/db-connection-info)
+(defn open-global-when-necessary
+  "Open a global connection only when necessary, that is, when no previous
+  connection exist or when db-spec is different to the current global
+  connection."
+  [db-spec]
+  ;; If the connection credentials has changed, close the connection.
+  (when (and (@lobos.connectivity/global-connections :default-connection)
+             (not= (:db-spec (@lobos.connectivity/global-connections :default-connection)) db-spec))
+    (lobos.connectivity/close-global))
+  ;; Open a new connection or return the existing one.
+  (if (nil? (@lobos.connectivity/global-connections :default-connection))
+    ((lobos.connectivity/open-global db-spec) :default-connection)
+    (@lobos.connectivity/global-connections :default-connection)))
+
+(open-global-when-necessary db/db-connection-info)
--- src/lobos/migrations.clj
+++ src/lobos/migrations.clj
@@ -1,7 +1,7 @@
 (ns lobos.migrations
   (:refer-clojure :exclude [alter drop
                             bigint boolean char double float time])
-  (:use (lobos [migration :only [defmigration]] core schema)))
+  (:use (lobos [migration :only [defmigration]] core schema) lobos.config))
 
 (defmigration create-thing
               (up [] (create(table :thing
