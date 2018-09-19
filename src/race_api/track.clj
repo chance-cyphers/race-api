0ac5ba93520cae9151f3c0906f51b42907a38dc7
chance <chance.a.cyphers@gmail.com>
2018-09-19 18:37:20 -0400

    Add id to track resource

--- src/race_api/track.clj
+++ src/race_api/track.clj
@@ -3,7 +3,8 @@
 
 (defn get-track [trackId]
   (let [track (query/get-track trackId) entrants (query/get-entrants-by-track trackId)]
-    {:status (:status track)
+    {:id (:id track)
+     :status (:status track)
      :entrants
              (map #(identity {:id     (:id %)
                               :userId (:userId %)}) entrants)}))
