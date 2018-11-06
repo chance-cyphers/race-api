9ab37f42c399ff91d3400b3cad8b908067b33c59
chance <chance.a.cyphers@gmail.com>
2018-11-06 15:20:40 -0500

    Return location update links with track

--- src/race_api/handlers/track.clj
+++ src/race_api/handlers/track.clj
@@ -1,8 +1,15 @@
 (ns race-api.handlers.track
-  (:require [race-api.db.query :as query]))
+  (:require [race-api.db.query :as query]
+            [race-api.config :as config]))
+
+(defn entrant-locations [entrants]
+  (let [host config/service-url]
+    (map #(str host "/entrant/" (:id %) "/location") entrants)))
 
 (defn get-track [trackId]
-  (let [track (first (query/get-tracks {:id trackId}))]
-    {:id (:id track)
-     :status (:status track)
-     :entrants (:entrant track)}))
+  (let [track (first (query/get-tracks {:id trackId}))
+        entrants (:entrant track)]
+    {:id       (:id track)
+     :status   (:status track)
+     :entrants entrants
+     :links    {:entrantLocations (entrant-locations entrants)}}))
