4c81d0577387594e986e83a0a1e007e805de0604
chance <chance.a.cyphers@gmail.com>
2018-11-06 15:38:24 -0500

    Refactor get track response function

--- src/race_api/handlers/track.clj
+++ src/race_api/handlers/track.clj
@@ -1,15 +1,18 @@
 (ns race-api.handlers.track
   (:require [race-api.db.query :as query]
-            [race-api.config :as config]))
+            [race-api.config :as config]
+            [ring.util.response :as response]))
 
 (defn entrant-location-links [entrants]
   (let [host config/service-url]
     (map #(str host "/entrant/" (:id %) "/location") entrants)))
 
+(defn response [track]
+  (let [entrants (:entrant track)]
+    (response/response {:id       (:id track)
+                        :status   (:status track)
+                        :entrants entrants
+                        :links    {:entrantLocations (entrant-location-links entrants)}})))
+
 (defn get-track [trackId]
-  (let [track (first (query/get-tracks {:id trackId}))
-        entrants (:entrant track)]
-    {:id       (:id track)
-     :status   (:status track)
-     :entrants entrants
-     :links    {:entrantLocations (entrant-location-links entrants)}}))
+  (response (first (query/get-tracks {:id trackId}))))
