5a37fc4c34505b7407debe0eb13b6bbcd9f910d6
chance <chance.a.cyphers@gmail.com>
2019-03-11 10:49:39 -0400

    Add swagger docs

--- start-pg.sh
+++ start-pg.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker run --rm --name racedb -e POSTGRES_DB=raceDb -e POSTGRES_USER=foo -e POSTGRES_PASSWORD=foo  -d -p 5432:5432 postgres
 echo "(use 'lobos.migrations) (run-migrations)" | lein repl
+
+
+docker exec -it raceDb bash
+psql raceDb -U foo
